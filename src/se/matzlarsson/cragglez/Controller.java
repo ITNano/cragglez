3bcd0007874d5ce8313f94539c837f7240328097
Matz Larsson <matzlarsson94@gmail.com>
2018-03-13 09:25:29 +0100

    Fix setup to my satisfaction

---
+++ src/se/matzlarsson/cragglez/Controller.java
@@ -0,0 +1,4 @@
+package se.matzlarsson.cragglez;
+
+public class Controller {
+}
--- src/se/matzlarsson/cragglez/Main.java
+++ src/se/matzlarsson/cragglez/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package se.matzlarsson.cragglez;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
